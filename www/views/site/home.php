cf85e5dd23ca838eaf6476995f8ae2fef1cf0e47
LarissaCarlos <mpkavolt.l@rede.ulbra.br>
2022-09-14 18:04:22 -0300

    first commit

---
+++ www/views/site/home.php
@@ -0,0 +1,4 @@
+    <h2> Home </h2>
+ <p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et blanditiis quasi officiis nulla animi ipsum corrupti ex ipsa, consequuntur esse est exercitationem, fuga repellat adipisci. Enim quidem et eius praesentium quibusdam at vitae accusamus error a? Quos ea voluptatum, quaerat autem reiciendis commodi id temporibus non voluptas ipsa blanditiis quae eum veritatis consequatur dolore illo reprehenderit harum cumque aperiam saepe nisi, nam nulla ad ipsum. Aspernatur beatae quia, quisquam, ex repellendus eveniet maxime saepe omnis dignissimos nihil molestiae, impedit unde nemo nulla perferendis? Cupiditate ex, odio hic modi aperiam, assumenda velit repudiandae dolor voluptas pariatur vero? Inventore magnam quidem ex.
+ </p>
---
+++ www/views/templates/footer.php
@@ -0,0 +1,12 @@
+
+            </section>
+        </div>
+    </div>
+
+    <footer class="p-3 bg-dark text-white text-center">
+        <h2>Rodapé</h2>
+        <a href= "../index.php">Site</a>
+    </footer> 
+</body>
+
+</html>
